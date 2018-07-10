83dfb670943286ddf83dae80a60873a0dfac82cc
Naia Crump <32797281+NaiaCrump-CodeNext@users.noreply.github.com>
2018-07-10 12:57:58 -0400

    Starting Pythagorean triple
    **if user#1 squared + user#2 squared = user#3 squred, then return true.

---
+++ pythagorean-challenge.js
@@ -0,0 +1,9 @@
+var readline = require("readline-sync");
+
+console.log("Welcome to the Pythagorean Triple Checker!")
+console.log("a^2 * b^2 = c^2")
+let userNumber1 = parseFloat(readline.question("Please enter the first value: "))
+let userNumber2 = parseFloat(readline.question("Please enter the second value: "))
+let userNumber3 = parseFloat(readline.question("Please enter the third value: "))
+
+if (userNumber1)
