15cd13adc4c6ef8a5fbf4d15fc56d7fd49e4cade
Naia Crump <32797281+NaiaCrump-CodeNext@users.noreply.github.com>
2018-07-10 11:30:09 -0400

    Debugging Super Easter Predictor
    Added a bit of "flair" and made it user input friendly.

--- user-input-practice.js
+++ user-input-practice.js
@@ -7,18 +7,18 @@ var readline = require("readline-sync");
 
 // 1. Ask the user to enter a number, and print out "The square root of ___ is ___ ", but with the blanks filled in correctly. (Not sure how to get the square root of a number in JavaScript? Google it!)
 
-// var numberUser = readline.question("Please enter a number: ")
-// console.log("The square root of " + numberUser + " is " + (Math.sqrt(numberUser)))
+var numberUser = readline.question("Please enter a number: ")
+console.log("The square root of " + numberUser + " is " + (Math.sqrt(numberUser)))
 
 // 2. Ask the user to enter their name, and print out "Your name has ___ letters", but with the blank filled in. Not sure how to get the number of letters in a string in JavaScript? Google it!
 
-// let userName = readline.question("Please enter your name: ");
-// console.log("Your name has " + userName.length + " letters." );
+let userName = readline.question("Please enter your name: ");
+console.log("Your name has " + userName.length + " letters." );
 
 // 3. Ask the user to enter any word, and print out "The word ____ starts with the letter ___", but with the blanks filled in. Not sure how to get the first letter of a string in JavaScript? ...
 
-// let userWord = readline.question("Please type in any word: ")
-// console.log("Oh I know! The word " + userWord + " starts with the letter " +  userWord.charAt(0))
+let userWord = readline.question("Please type in any word: ")
+console.log("Oh I know! The word " + userWord + " starts with the letter " +  userWord.charAt(0))
 
 // 4. Ask the user to enter two numbers, and print out "The average of ___ and ___ is ____", but with the blanks filled in.
 
