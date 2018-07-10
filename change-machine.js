d2987090631cbf1184919b64a88201a162a32514
Naia Crump <32797281+NaiaCrump-CodeNext@users.noreply.github.com>
2018-07-10 12:20:22 -0400

    Completed Change Machine
    A change machine that uses division and remainders to give accurate change for any postive interger given.

--- change-machine.js
+++ change-machine.js
@@ -1,1 +1,33 @@
-// Author: FirstName LastName
+// Author: Naia Crump
+
+let readline = require("readline-sync");
+console.log("*****************************************************************")
+
+console.log("bLeep blOop! i'm a CHanGE macHINe!")
+let userMoney = readline.question("Please enter your amount of cents, and I'll give you back change:  ")
+
+console.log(userMoney + " cents makes:")
+
+let quarters = Math.floor(userMoney / 25);
+let remandier = Math.floor(userMoney % 25);
+
+console.log("Quarters: " + quarters);
+
+let dimes = Math.floor(remandier / 10);
+let remandierDime = Math.floor(remandier % 10);
+
+console.log("Dimes: " + dimes)
+
+let nickles = Math.floor(remandierDime / 5);
+let remandierNick = Math.floor(remandierDime % 5); 
+
+console.log("Nickles: " + nickles)
+
+
+let pennies = Math.floor(remandierNick / 1);
+
+console.log("Pennies: " + pennies)
+
+
+console.log("tHanKS for UsINg BleEp BloOp!")
+console.log("*****************************************************************")
