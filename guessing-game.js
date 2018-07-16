031dda04247b1318f4013ea0bc5abbef910c0bb8
Naia Crump <32797281+NaiaCrump-CodeNext@users.noreply.github.com>
2018-07-16 16:26:23 -0400

    Added Semi-Colons to Guessing Game
    -.-

--- guessing-game.js
+++ guessing-game.js
@@ -10,29 +10,28 @@ var readline = require("readline-sync");
 
 // Set a random number from 1 to 100 to be the answer.
 let correctAnswer = randomInt(1, 100);
-let howManyGuesses = 0 
+let howManyGuesses = 0;
 // Ask the user to guess a number.
-let userGuess = readline.question("Please guess a number between 1 and 100: ")
+let userGuess = readline.question("Please guess a number between 1 and 100: ");
 // If they're wrong, you should ask them to guess again.
 
 while (userGuess != correctAnswer){
-	console.log("Sorry, that's not the right answer. Try Again.")
-	howManyGuesses++
-	console.log("You've guessed incorrectly " + howManyGuesses + " time(s).")
+	console.log("Sorry, that's not the right answer. Try Again.");
+	howManyGuesses++;
+	console.log("You've guessed incorrectly " + howManyGuesses + " time(s).");
 	if (userGuess > correctAnswer){
-		console.log("That guess is too high!")
+		console.log("That guess is too high!");
 	} else if (userGuess < correctAnswer){
-		console.log("That guess is much too low.")
+		console.log("That guess is much too low.");
 	}
-	userGuess = readline.question("Please guess another number: ")	
+	userGuess = readline.question("Please guess another number: ");	
 }
 
 if (howManyGuesses <= 5){ 
-	console.log("It took you " + howManyGuesses + " guesses! That's amazing!")
+	console.log("It took you " + howManyGuesses + " guesses! That's amazing!");
 } else if (howManyGuesses >= 6){
-	console.log("It took you " + howManyGuesses + " guesses. Oof.")
 }
-console.log("Congradulations! " + correctAnswer + " was the right guess!")
+console.log("Congradulations! " + correctAnswer + " was the right guess!");
 
 // Each time they guess, you should be telling them if their guess was too high or too low.
 // Also, you should be tracking how many times they have guessed.
